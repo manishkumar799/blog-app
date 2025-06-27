869d68f10e97247ab96d0f122050054be2eb0533
Manish <manishkr9900@gmail.com>
2025-06-28 03:03:15 +0530

    Change database sync option from 'force' to 'alter'

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { sequelize } = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 // Sync database
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
   console.log('Database synced');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
